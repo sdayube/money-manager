b75e57efc02b1934fc8272d8f333d35ab94a15fe
Silvio Dayube <scarige@gmail.com>
2022-02-21 12:33:37 -0300

    Add axios library

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,14 +1,17 @@
 import { useEffect } from 'react';
+
 import { formatCurrency } from '../../helpers/formatCurrency';
+import { api } from '../../services/api';
+
 import { Container } from './styles';
 
 
 export function TransactionsTable() {
   const [desenvolvimento, aluguel] = [1200000, -300000];
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-      .then(response => response.json())
-      .then(data => console.log(data));
+    api.get('transactions').then(response => { 
+      console.log(response.data);
+    });
   }, []);
 
   return (
